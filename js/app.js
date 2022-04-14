33e8954477d24e67bf1298ee467a09752c0eaa09
Nguyen <tranchauthanhnguyen@gmail.com>
2022-04-15 04:03:34 +0700

    update code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 var clickMe = document.querySelector('#clickMe')
 var reset = document.querySelector('#resetForm')
 var body = document.querySelector('body')
+var content = document.querySelector("#content");
 var listColor = ['red', 'blue', 'green', 'yellow', 'pink', 'grey', 'lime']
 body.style.backgroundColor = 'black'
 
 reset.classList.add('none')
 
 clickMe.addEventListener('click', function(){
-    var changeClor = parseInt(Math.random() * listColor.length)
-    body.style.backgroundColor = listColor[changeClor]
+    var changeColor = parseInt(Math.random() * listColor.length)
+    body.style.backgroundColor = listColor[changeColor]
     body.style.transition = '1s linear'
     reset.classList.remove('none')
+    content.style.justifyContent = "space-between"
 })
 
 reset.addEventListener('click', function(){
-    location.reload();
+    body.style.backgroundColor = 'black'
+    body.style.transition = '1s'
+    reset.classList.add('none')
+    content.style.justifyContent = "center"
 })
